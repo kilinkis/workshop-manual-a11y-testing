950a83a4ec651b48c70da89c771a75993ddf7af8
Juan Manuel Incaurgarat <juanofx@gmail.com>
2022-10-02 15:09:12 +0200

    Fixes keyboard focus states

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,21 @@
 function load() {
-    const menuItems = Array.from(document.querySelectorAll('.megamenu-navitem'))
-    const submenus = Array.from(document.querySelectorAll('.megamenu-section'))
+    const menuItems = document.querySelectorAll('.megamenu-navitem')
+    const submenus = document.querySelectorAll('.megamenu-section')
 
-    function resetSubmenus(event) {
+    const resetSubmenus = (event) =>  {
         var activeSubmenu = document.querySelector('.megamenu-section.active')
         if (activeSubmenu && !activeSubmenu.contains(event.target)) {
             activeSubmenu.classList.remove('active')
         }
     }
 
-    menuItems.forEach((menuItem, index) => {
+    menuItems.forEach((menuItem) => {
         let parentElement = menuItem.parentNode
+
+        /**
+         * Since we are using buttons for the nav items, both mouse clicks and pressing
+         * the Enter key or Space bar will be recognized by our click event listener.
+         */
         menuItem.addEventListener('click', function(event) {
             console.log(menuItem)
             resetSubmenus(event)
@@ -18,12 +23,20 @@ function load() {
             // Add active menu for the item clicked
             if(!parentElement.classList.contains('active')) {
                 parentElement.classList.add('active')
-
             } else {
                 parentElement.classList.remove('active')
             }
         })
     })
+
+    submenus.forEach((submenu) => {
+        submenu.addEventListener('keyup', (event) => {
+          if (event.key === 'Escape' && submenu.classList.contains('active')) {
+            submenu.classList.remove('active')
+            submenu.querySelector('.megamenu-navitem').focus()
+          }
+        })
+      })
 
     document.addEventListener('click', (event) => {
         resetSubmenus(event)
